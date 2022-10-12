58005930b872597a5039c2935c36ef26c7897008
Xifre Font <xifre.font.97@gmail.com>
2022-10-12 17:01:41 +0200

    Installed and imported Manrope font

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
+import "@fontsource/manrope/400.css";
+import "@fontsource/manrope/600.css";
+import "@fontsource/manrope/800.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
