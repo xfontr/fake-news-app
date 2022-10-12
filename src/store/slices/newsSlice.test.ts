88011fea0d314391443963ca5214826a9efd7c16
Xifre Font <xifre.font.97@gmail.com>
2022-10-12 20:50:17 +0200

    Tested the news reducer with a fake action and with the load action

--- src/store/slices/newsSlice.test.ts
+++ src/store/slices/newsSlice.test.ts
@@ -1,5 +1,5 @@
-import { mockNewsList } from "../../test-utils/mocks/mockNews";
-import { loadAllActionCreator } from "./newsSlice";
+import { mockNews, mockNewsList } from "../../test-utils/mocks/mockNews";
+import { loadAllActionCreator, newsReducer } from "./newsSlice";
 
 describe("Given a loadAll action creator", () => {
   describe("When called with a list of news as payload", () => {
@@ -15,6 +15,37 @@ describe("Given a loadAll action creator", () => {
       const createdAction = loadAllActionCreator(mockNewsList);
 
       expect(createdAction).toStrictEqual(action);
+    });
+  });
+});
+
+describe("Given a newsReducer function", () => {
+  describe("When called with a fake action", () => {
+    test("Then it should return the same current state", () => {
+      const initialState = mockNewsList;
+
+      const fakeAction = {
+        type: "fakeAction/loadAll",
+        payload: "",
+      };
+
+      const result = newsReducer(initialState, fakeAction);
+
+      expect(result).toStrictEqual(initialState);
+    });
+  });
+
+  describe("When called with a loadAll action", () => {
+    test("Then it should replace the previous news with the passed ones", () => {
+      const initialState = mockNewsList;
+      const newList = [...mockNewsList, { ...mockNews, id: 3 }];
+      const loadAllAction = loadAllActionCreator(newList);
+
+      const expectedNewsList = newList;
+
+      const result = newsReducer(initialState, loadAllAction);
+
+      expect(result).toStrictEqual(expectedNewsList);
     });
   });
 });
