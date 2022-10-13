eecd65b7fac17c0bcaa3e9e50e891fab1a7e1600
Xifre Font <xifre.font.97@gmail.com>
2022-10-13 10:14:30 +0200

    Tested the case that get is called with an empty object

--- src/utils/api.test.ts
+++ src/utils/api.test.ts
@@ -8,8 +8,21 @@ jest.mock("axios", () => ({
 }));
 
 describe("Given a get function returned by an api function", () => {
+  describe("When called only with an url", () => {
+    test("Then it should call the module that fetches the url with an empty config object", () => {
+      const url = "";
+      const config = {};
+
+      const { get } = api();
+
+      get(url);
+
+      expect(mockGet).toHaveBeenCalledWith(url, config);
+    });
+  });
+
   describe("When called with an url and a config object", () => {
-    test("Then it should call the module that fetches the url", () => {
+    test("Then it should call the module that fetches the url with the config object", () => {
       const url = "";
       const config = {
         headers: {},
