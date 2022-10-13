ba6af7ace31e3a512dd77c75dd6d25ba56c74665
Xifre Font <xifre.font.97@gmail.com>
2022-10-13 11:54:49 +0200

    Sidebar html skeleton

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+const Sidebar = (): JSX.Element => (
+  <aside className="sidebar">
+    <h3 className="sidebar__title">Lorem ipsum nemo</h3>
+    <p className="sidebar__body">
+      Velit neque libero incidunt itaque impedit vitae quam, architecto autem
+      reprehenderit?
+    </p>
+    <button className="sidebar__cta">Lorem now</button>
+  </aside>
+);
+
+export default Sidebar;
