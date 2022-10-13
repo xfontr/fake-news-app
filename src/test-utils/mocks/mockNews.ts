8378b468c45affe721daf7baf1acb161e5dee63d
Xifre Font <xifre.font.97@gmail.com>
2022-10-13 19:22:40 +0200

    Tested the capitalize functions

--- src/test-utils/mocks/mockNews.ts
+++ src/test-utils/mocks/mockNews.ts
@@ -3,13 +3,13 @@ import News from "../../types/News";
 export const mockNews: News = {
   userId: 1,
   id: 1,
-  title: "News title",
-  body: "Random news description",
+  title: "news title",
+  body: "random news description",
 };
 
 export const mockNewsList: News[] = [
   { ...mockNews },
-  { ...mockNews, userId: 2, id: 2, title: "News title 2" },
+  { ...mockNews, userId: 2, id: 2, title: "news title 2" },
 ];
 
 export const mockNewsWithAuthor: News = {
