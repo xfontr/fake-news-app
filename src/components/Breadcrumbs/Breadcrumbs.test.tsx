46e80dd2afad1ff26bf2a9b069a751a9680bdfb6
Xifre Font <xifre.font.97@gmail.com>
2022-10-14 11:35:48 +0200

    Tested the breadcrumbs component

---
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -0,0 +1,30 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import { store } from "../../app/store";
+import { screen } from "../../test-utils/customTestingLibrary";
+import Breadcrumbs from "./Breadcrumbs";
+
+describe("Given a Breadcrumbs component", () => {
+  describe("When instantiated and the route is '/news/fakepage'", () => {
+    test("Then it should display a link to home and fakepage, and a text '/news'", () => {
+      const falsePath = "/news/fakepage";
+
+      render(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={[falsePath]}>
+            <Breadcrumbs />
+          </MemoryRouter>
+        </Provider>
+      );
+
+      const breadcrumbs = [
+        screen.getByRole("link", { name: "FakeNews" }),
+        screen.getByRole("link", { name: "/ news" }),
+        screen.getByText("/ fakepage"),
+      ];
+
+      breadcrumbs.forEach((path) => expect(path).toBeInTheDocument());
+    });
+  });
+});
