da1a6d86bca6ba098144da489d0f02029df5f2a2
Xifre Font <xifre.font.97@gmail.com>
2022-10-14 11:46:54 +0200

    Implemented a delete button on each news card

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,3 +1,5 @@
+import { useAppDispatch } from "../../app/hooks";
+import { deleteActionCreator } from "../../store/slices/newsSlice";
 import INews from "../../types/News";
 import Button from "../Button/Button";
 
@@ -5,15 +7,28 @@ type NewsProps = {
   news: INews;
 };
 
-const News = ({ news: { title, author, body } }: NewsProps): JSX.Element => (
-  <article className="news">
-    <span className="news__author">{author}</span>
-    <h3 className="news__title">{title}</h3>
-    <p className="news__body">{body}</p>
-    <div className="news__options">
-      <Button className="news__cta">Full article</Button>
-    </div>
-  </article>
-);
+const News = ({
+  news: { title, author, body, id },
+}: NewsProps): JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteActionCreator(id));
+  };
+
+  return (
+    <article className="news">
+      <span className="news__author">{author}</span>
+      <h3 className="news__title">{title}</h3>
+      <p className="news__body">{body}</p>
+      <div className="news__options">
+        <Button className="news__delete" onClick={handleDelete}>
+          Delete
+        </Button>
+        <Button className="news__cta">Full article</Button>
+      </div>
+    </article>
+  );
+};
 
 export default News;
