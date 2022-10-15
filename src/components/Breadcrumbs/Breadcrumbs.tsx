072ce7d3a7c467c5141640016234570b9844803d
Xifre Font <xifre.font.97@gmail.com>
2022-10-15 16:27:32 +0200

    Update form can be submitted and updates the actual news

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import paths from "../../config/paths";
 
@@ -6,10 +5,6 @@ const Breadcrumbs = (): JSX.Element => {
   const { pathname } = useLocation();
   const userPaths = pathname.split("/");
   userPaths.splice(0, 1);
-
-  useEffect(() => {
-    console.log(pathname);
-  }, [pathname]);
 
   return (
     <div className="breadcrumbs">
--- src/pages/UpdatePage/UpdatePage.tsx
+++ src/pages/UpdatePage/UpdatePage.tsx
@@ -1,14 +1,18 @@
+import { SyntheticEvent } from "react";
 import { useParams } from "react-router-dom";
-import { useAppSelector } from "../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import Button from "../../components/Button/Button";
 import Form from "../../components/Form/Form";
 import paths from "../../config/paths";
 import updateNewsForm from "../../data/updateNewsForm.schema";
 import useForm from "../../hooks/useForm";
+import { updateActionCreator } from "../../store/slices/newsSlice";
+import News from "../../types/News";
 import { getCurrentNews, setSchema } from "../../utils/updateUtils/updateUtils";
 
 const UpdatePage = (): JSX.Element => {
   const { news } = useAppSelector((state) => state);
+  const dispatch = useAppDispatch();
   const { id } = useParams();
 
   const currentNews = getCurrentNews(+id!, news);
@@ -16,10 +20,25 @@ const UpdatePage = (): JSX.Element => {
 
   const { loadProps, values } = useForm(schema);
 
+  const handleUpdate = (event: SyntheticEvent): void => {
+    event.preventDefault();
+
+    const finalNews: News = {
+      ...currentNews!,
+      title: values.title as string,
+      body: values.body as string,
+      author: values.author as string,
+    };
+
+    dispatch(updateActionCreator(finalNews));
+  };
+
   return (
     <section className="update">
       {currentNews ? (
-        <Form {...{ loadProps, values, schema }} />
+        <Form {...{ loadProps, values, schema }} onSubmit={handleUpdate}>
+          <Button>Update</Button>
+        </Form>
       ) : (
         <div>
           <span className="update__information">
