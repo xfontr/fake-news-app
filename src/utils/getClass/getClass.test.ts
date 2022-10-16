bb4321902839ab5b812ba3b31a99f9c5c79c3abd
Xifre Font <xifre.font.97@gmail.com>
2022-10-16 14:25:30 +0200

    Function to get the class with the error, and tested it

--- src/utils/getClass/getClass.test.ts
+++ src/utils/getClass/getClass.test.ts
@@ -1,4 +1,6 @@
-import getClass from "./getClass";
+import { ValidationResult } from "joi";
+import updateNewsForm from "../../data/updateNewsForm.schema";
+import { getClass, getFullClass } from "./getClass";
 
 describe("Given a getClass function", () => {
   describe("When called with a class 'test' and an additional class 'test--extra'", () => {
@@ -21,6 +23,54 @@ describe("Given a getClass function", () => {
       const result = getClass(defaultClass, additionalClass);
 
       expect(result).toBe(defaultClass);
+    });
+  });
+});
+
+describe("Given a getFullClass function", () => {
+  describe("When called with a list of errors and a form field that matches with the error", () => {
+    test("Then it should return 'form__input form__input--error'", () => {
+      const errors = {
+        error: {
+          details: [
+            {
+              path: [updateNewsForm[0].id],
+              message: "Name must be 3 at least 3 characters long",
+            },
+          ],
+        },
+      };
+      const expectedClass = "form__input form__input--error";
+
+      const result = getFullClass(
+        errors as ValidationResult<unknown>,
+        updateNewsForm[0]
+      );
+
+      expect(result).toBe(expectedClass);
+    });
+  });
+
+  describe("When called with a list of errors and a form field that doesn't match with the error", () => {
+    test("Then it should return 'form__input'", () => {
+      const errors = {
+        error: {
+          details: [
+            {
+              path: [updateNewsForm[1].id],
+              message: "Name must be 3 at least 3 characters long",
+            },
+          ],
+        },
+      };
+      const expectedClass = "form__input";
+
+      const result = getFullClass(
+        errors as ValidationResult<unknown>,
+        updateNewsForm[0]
+      );
+
+      expect(result).toBe(expectedClass);
     });
   });
 });
