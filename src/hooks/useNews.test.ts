303624cddfeee1773c65c20808ef0eb670b309e9
Xifre Font <xifre.font.97@gmail.com>
2022-10-17 15:43:26 +0200

    Tested the ui calls for the loading action

--- src/hooks/useNews.test.ts
+++ src/hooks/useNews.test.ts
@@ -1,5 +1,6 @@
 import { act } from "react-dom/test-utils";
 import { loadAllActionCreator } from "../store/slices/newsSlice";
+import { setUiActionCreator, UiState } from "../store/slices/uiSlice";
 import { renderHook } from "../test-utils/customTestingLibrary";
 import { mockAuthorList } from "../test-utils/mocks/mockAuthor";
 import { mockNewsList } from "../test-utils/mocks/mockNews";
@@ -14,6 +15,28 @@ jest.mock("../store/hooks", () => ({
 }));
 
 describe("Given a getAll function returned from a useNews function", () => {
+  describe("When called", () => {
+    test("Then it should open the ui modal with a 'Loading...' message", async () => {
+      const expectedModal: UiState = {
+        status: "LOADING",
+        message: "Loading the latest news",
+      };
+
+      const {
+        result: {
+          current: { getAll },
+        },
+      } = renderHook(useNews);
+
+      await act(() => {
+        getAll();
+      });
+
+      const calledWith = mockUseAppDispatch.mock.calls[0][0];
+      expect(calledWith).toStrictEqual(setUiActionCreator(expectedModal));
+    });
+  });
+
   describe("When called and the api responds with a list of news", () => {
     test("Then it should load the store with all the news with the author names", async () => {
       const {
@@ -35,10 +58,35 @@ describe("Given a getAll function returned from a useNews function", () => {
 
       expect(mockUseAppDispatch).toHaveBeenCalledWith(expectedAction);
     });
+
+    test("Then it should open the ui modal with a success message", async () => {
+      const {
+        result: {
+          current: { getAll },
+        },
+      } = renderHook(useNews);
+
+      const expectedModal: UiState = {
+        status: "SUCCESS",
+        message: "News loaded",
+      };
+
+      await act(() => {
+        getAll();
+      });
+
+      const calledWith = mockUseAppDispatch.mock.calls[2][0];
+      expect(calledWith).toStrictEqual(setUiActionCreator(expectedModal));
+    });
   });
 
   describe("When called and the news api responds with an error", () => {
-    test("Then it should do nothing", async () => {
+    test("Then it should call the ui modal with an error message", async () => {
+      const expectedModal: UiState = {
+        status: "ERROR",
+        message: "Failed to load the news",
+      };
+
       const uiDispatchCalls = 2;
       mockNewsList[0].title = "error";
 
@@ -53,6 +101,9 @@ describe("Given a getAll function returned from a useNews function", () => {
       });
 
       expect(mockUseAppDispatch).toHaveBeenCalledTimes(uiDispatchCalls);
+
+      const calledWith = mockUseAppDispatch.mock.calls[1][0];
+      expect(calledWith).toStrictEqual(setUiActionCreator(expectedModal));
     });
   });
 
