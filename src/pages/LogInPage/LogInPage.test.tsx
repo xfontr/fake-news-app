c46f0da2b3fa474a7edc1fd71b043016fd571d66
Xifre Font <xifre.font.97@gmail.com>
2022-10-18 09:03:51 +0200

    Unit tested the log in form redirection and ui modal message

--- src/pages/LogInPage/LogInPage.test.tsx
+++ src/pages/LogInPage/LogInPage.test.tsx
@@ -1,7 +1,23 @@
 import userEvent from "@testing-library/user-event";
+import paths from "../../config/paths";
 import logInForm from "../../data/logInForm.schema";
+import { setUiActionCreator, UiState } from "../../store/slices/uiSlice";
 import { render, screen } from "../../test-utils/customTestingLibrary";
 import LogInPage from "./LogInPage";
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
+const mockDispatch = jest.fn();
+
+jest.mock("../../store/hooks", () => ({
+  ...jest.requireActual("../../store/hooks"),
+  useAppDispatch: () => mockDispatch,
+}));
 
 describe("Given a LogInPage component", () => {
   describe("When instantiated", () => {
@@ -17,8 +33,8 @@ describe("Given a LogInPage component", () => {
       logIn.forEach((node) => expect(node).toBeInTheDocument());
     });
 
-    describe("And the user fills the forms", () => {
-      test("Then the fields values should change", async () => {
+    describe("And the user fills the forms with invalid data and submits", () => {
+      test("Then the fields values should change, but shouldn't redirect the user", async () => {
         const typedMessage = "Hello";
 
         render(<LogInPage />);
@@ -33,6 +49,36 @@ describe("Given a LogInPage component", () => {
 
         expect(email).toHaveValue(typedMessage);
         expect(password).toHaveValue(typedMessage);
+
+        const submit = screen.getByRole("button", { name: "Log in" });
+        await userEvent.click(submit);
+
+        expect(mockNavigate).not.toHaveBeenCalled();
+      });
+    });
+
+    describe("And the user fills the forms and submits", () => {
+      test("Then it should display a success modal and redirect to root", async () => {
+        const modal: UiState = {
+          status: "SUCCESS",
+          message: "User logged in",
+        };
+
+        render(<LogInPage />);
+
+        const [email, password] = [
+          screen.getByLabelText(logInForm[0].label),
+          screen.getByLabelText(logInForm[1].label),
+        ];
+
+        await userEvent.type(email, "hello@gmail.com");
+        await userEvent.type(password, "admin1234");
+
+        const submit = screen.getByRole("button", { name: "Log in" });
+        await userEvent.click(submit);
+
+        expect(mockNavigate).toHaveBeenCalledWith(paths.root);
+        expect(mockDispatch).toHaveBeenCalledWith(setUiActionCreator(modal));
       });
     });
   });
